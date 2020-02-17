296ead7cdd6ac444b3c692088dc8232954c6eef4
FIRDOSH PATEL <firdosh.patel@gmail.com>
2020-02-16 19:35:57 -0600

    uploading cleaned up versions of the scripts

--- PyElections/main_PyElections.py
+++ PyElections/main_PyElections.py
@@ -1,29 +1,22 @@
 #==========================================================================================================
-# This block of logic imports the relevant Python Modules
+# This block of logic imports the relevant Python Modules.
 #==========================================================================================================
 
 import os 
 import csv
-
 import collections
 import operator 
-# from collections import OrderedDict
-# from operator import itemgetter
-
-
 
 
 #==========================================================================================================
-# This block of logic create a Variable for the CSV File to be read
+# This block of logic creates a variable for the CSV File to be read.
 #==========================================================================================================
 
 my_csv_read_from_file = os.path.join('C:/Users/firdo/Desktop/CSV_TEST','HW3_Python_houston_election_data.csv')
 
 
-
-
 #==========================================================================================================
-# This block of logic defines the variables to be used in the analysis
+# This block of logic defines the variables to be used in the analysis.
 #==========================================================================================================
 
 my_current_row_candidate = 0
@@ -31,26 +24,23 @@ my_candidate_list = []
 my_candidate_votes = {}
 
 my_final_results = {}
-my_total_votes_final_results = 0
 
+my_total_votes_final_results = 0
 my_candidate_name_final_results = ()
 my_candidate_votecount_final_results = ()
 my_candidate_percentage_final_results = ()
 my_combined_final_results = ()
 
 
-
 #==========================================================================================================
 # This block of logic does 3 things:
-# 1. It uses the preferred and improved method of opening CSV files by using "with open" instead of just open 
-# 2. It ensures that rows with bad unreadable characters will not be skipped by specifying "encoding="utf8" 
-# 3. It reads the CSV file into a Dictionary by using csv.DictReader isntead of just csv.Reader
+# 1. It uses the preferred and improved method of opening CSV files by using "with open" instead of just open. 
+# 2. It ensures that rows with unreadable characters will not be skipped by specifying "encoding="utf8". 
+# 3. It reads the CSV file into a Dictionary by using csv.DictReader instead of just csv.Reader.
 #==========================================================================================================
 
 with open(my_csv_read_from_file, encoding="utf8", newline='') as election_data:
     my_csv_reader = csv.DictReader(election_data, delimiter=',')
-
-
 
 
 #==========================================================================================================
@@ -79,12 +69,12 @@ with open(my_csv_read_from_file, encoding="utf8", newline='') as election_data:
 
 #==========================================================================================================
 # This block of logic does 6 things:
-# 1. It creates a new DICTIONARY called final_results which sorts based on the Total Votes (VALUE) from my_candidate_votes 
-# 2. It then calculates the Total Votes cast for all candidates
-# 3. It cretaes a new list just with the Candidate Names (sorted based on Total Vote Count).
-# 4. It creates a new list just with the Total Vote Count (sorted based on Total Vote Count).
-# 5. It craetes a new list just with the Percentage of the Total Vote Cpount (sorted based on Total Vote Count).
-# 6. It then combines the 3 lists mentioned above into one combined list using the zip feature. 
+# 1. It creates a new DICTIONARY called final_results which sorts based on the Total Votes (VALUE) from my_candidate_votes. 
+# 2. It then calculates the Total Votes cast for all candidates.
+# 3. It cretaes a new LIST just with the Candidate Names (sorted based on Total Vote Count).
+# 4. It creates a new LIST just with the Total Vote Count (sorted based on Total Vote Count).
+# 5. It craetes a new LIST just with the Percentage of the Total Vote Count (sorted based on Total Vote Count).
+# 6. It then combines the 3 lists mentioned above into one combined LIST using the zip feature. 
 #===========================================================================================================
 
     my_final_results = sorted(my_candidate_votes.items(), key=operator.itemgetter(1),reverse=True)
@@ -101,14 +91,11 @@ with open(my_csv_read_from_file, encoding="utf8", newline='') as election_data:
 
     my_combined_final_results = zip(my_candidate_name_final_results,my_candidate_votecount_final_results,my_candidate_percentage_final_results)
 
-
+    
 #===========================================================================================================
-# This block of logic prints the results of the analysis to the Terminal
+# This block of logic prints the results of the analysis to the Terminal.
 #===========================================================================================================
-  
- 
-    print()
-    print()
+   
     print()
     print("--------------------------------------------------------")
     print("Houston Mayoral Election Results")
@@ -127,31 +114,32 @@ with open(my_csv_read_from_file, encoding="utf8", newline='') as election_data:
     print("--------------------------------------------------------")
 
 
-
-
 #===========================================================================================================
 # This block of logic does 3 things to write the output to a CSV File:
 # 1. It re-zips 3 lists into the combined list. 
-# For some reason when the original zipped list was printed, it was no longer available to be used.
-# 2. It creates a variable for the output CSV File
-# 3. It reads from the re-zipped list and writes to the output CSV file.
+# When the original zipped combined list was printed, it was no longer available to be used.
+# 2. It creates a variable for the output CSV File.
+# 3. It reads from the re-zipped combined list and writes to the output CSV file.
 #===========================================================================================================
-  
-
+ 
 my_combined_final_results = zip(my_candidate_name_final_results,my_candidate_votecount_final_results,my_candidate_percentage_final_results)
-
 
 output_file = os.path.join('C:/Users/firdo/Desktop/CSV_TEST', 'election_data_analysis_output.csv')
 with open(output_file, 'w') as datafile:
     writer = csv.writer(datafile) 
     writer.writerow(["Candidate Name", "Total Votes Received", "Percentage of Votes Received"])
     writer.writerows(my_combined_final_results)
-
-
+    writer.writerow(["",""])
+    writer.writerow(["1st Advancing Candidate :", str(my_candidate_name_final_results[0])])
+    writer.writerow(["2nd Advancing Candidate :", str(my_candidate_name_final_results[1])])
 
 
 #===========================================================================================================
-# This block of logic writes the output to a TXT file:
+# This block of logic does 3 things to write the output to a TXT File:
+# 1. It re-zips 3 lists into the combined list. 
+# When the original zipped combined list was printed, it was no longer available to be used.
+# 2. It creates a variable for the output TXT File.
+# 3. It reads from the re-zipped combined list and writes to the output TXT file.
 #===========================================================================================================
   
 my_combined_final_results = zip(my_candidate_name_final_results,my_candidate_votecount_final_results,my_candidate_percentage_final_results)
@@ -172,7 +160,16 @@ with open(output_file, "w") as datafile:
     for r in my_combined_final_results:
         datafile.write((str(r[0]) + " : " + str('{0:.2%}'.format(r[2])) + "  (" + str('{:,}'.format(r[1])) + ")" ))
         datafile.write("\n")  
-    
-    
+    datafile.write("--------------------------------------------------------")
+    datafile.write("\n")  
+    datafile.write("1st Advancing Candidate  :  " + str(my_candidate_name_final_results[0]))
+    datafile.write("\n") 
+    datafile.write("2nd Advancing Candidate  :  " + str(my_candidate_name_final_results[1]))
+    datafile.write("\n") 
+    datafile.write("--------------------------------------------------------")
+    datafile.write("\n") 
 
 
+#===========================================================================================================
+# This is the end of the script. 
+#===========================================================================================================
--- PyFinances/main_PyFinances.py
+++ PyFinances/main_PyFinances.py
@@ -1,27 +1,22 @@
 #==========================================================================================================
-# This block of logic imports the relevant Python Modules
+# This block of logic imports the relevant Python Modules.
 #==========================================================================================================
 
 import os 
 import csv
-
 import collections
 import operator 
-# from collections import OrderedDict
-# from operator import itemgetter
-
 
 
 #==========================================================================================================
-# This block of logic create a Variable for the CSV File to be read
+# This block of logic create a variable for the CSV File to be read.
 #==========================================================================================================
 
 my_csv_read_from_file = os.path.join('C:/Users/firdo/Desktop/CSV_TEST','HW3_Python_budget_data.csv')
 
 
-
 #==========================================================================================================
-# This block of logic defines the variables to be used in the analysis
+# This block of logic defines the variables to be used in the analysis.
 #==========================================================================================================
 
 my_current_row_month = 0
@@ -44,27 +39,25 @@ my_month_with_greatest_increase_in_profits = 0
 my_month_with_greatest_decrease_in_profits = 0
 
 
-
 #==========================================================================================================
 # This block of logic does 3 things:
-# 1. It uses the preferred and improved method of opening CSV files by using "with open" instead of just open 
-# 2. It ensures that rows with bad unreadable characters will not be skipped by specifying "encoding="utf8" 
-# 3. It reads the CSV file into a Dictionary by using csv.DictReader isntead of just csv.Reader
+# 1. It uses the preferred and improved method of opening CSV files by using "with open" instead of just open. 
+# 2. It ensures that rows with unreadable characters will not be skipped by specifying "encoding="utf8". 
+# 3. It reads the CSV file into a Dictionary by using csv.DictReader instead of just csv.Reader.
 #==========================================================================================================
 
 with open(my_csv_read_from_file, encoding="utf8", newline='') as budget_data:
     my_csv_reader = csv.DictReader(budget_data, delimiter=',')
 
 
-
 #==========================================================================================================
 # This block of logic does 6 things:
-# 1. It identifies the current row of the csv reader that the loop is on in a variable called my_current_row_month
-# 2. It calculates the Total number of Months
-# 3. It calculates the Total Profit/Loss for all the months
-# 4. It calculates the Average Change in the Profit/Loss from one month to the next
-# 5. It calculates the Greatest Increase in the Profit/Loss from one month to the next and identifies the Month
-# 6. It calculates the Greatest Decrease in the Profit/Loss from one month to the next and identifies the Month
+# 1. It identifies the current row of the csv reader that the loop is on in a variable called my_current_row_month.
+# 2. It calculates the Total number of Months.
+# 3. It calculates the Total Profit/Loss for all the months.
+# 4. It calculates the Average Change in the Profit/Loss from one month to the next.
+# 5. It calculates the Greatest Increase in the Profit/Loss from one month to the next and identifies the Month.
+# 6. It calculates the Greatest Decrease in the Profit/Loss from one month to the next and identifies the Month.
 #==========================================================================================================
 
     for row in my_csv_reader:
@@ -94,19 +87,12 @@ with open(my_csv_read_from_file, encoding="utf8", newline='') as budget_data:
         if int(my_monthly_pnl_change_today) < int(my_greatest_decrease_in_profits): 
             my_greatest_decrease_in_profits = my_monthly_pnl_change_today
             my_month_with_greatest_decrease_in_profits = row["Date"] 
-
-
-           
-        
-
+   
     
 #===========================================================================================================
-# This block of logic prints the results of the analysis to the Terminal
+# This block of logic prints the results of the analysis to the Terminal.
 #===========================================================================================================
-  
- 
-    print()
-    print()
+   
     print()
     print("--------------------------------------------------------")
     print("Financial Analysis")
@@ -118,26 +104,22 @@ with open(my_csv_read_from_file, encoding="utf8", newline='') as budget_data:
     print("Greatest Decrease in Profits: " + my_month_with_greatest_decrease_in_profits + " : " + str('${:,}'.format(my_greatest_decrease_in_profits)))
     print("--------------------------------------------------------")
 
-
   
 #===========================================================================================================
-# This block of logic writes the output to a CSV file:
+# This block of logic writes the output to a CSV file.
 #===========================================================================================================
   
-
 output_file = os.path.join('C:/Users/firdo/Desktop/CSV_TEST', 'budget_data_analysis_output.csv')
 with open(output_file, 'w') as datafile:
     writer = csv.writer(datafile) 
     writer.writerow(["Total Months", "Total Profit/Loss", "Average Change", "Greatest Increase in Profits", "Greatest Decrease in Profits"])
-    writer.writerow(["","","",my_month_with_greatest_increase_in_profits,my_month_with_greatest_decrease_in_profits])
     writer.writerow([my_total_months,my_total_pnl_today,my_average_monthly_pnl_change,my_greatest_increase_in_profits,my_greatest_decrease_in_profits])
-
+    writer.writerow(["","","",my_month_with_greatest_increase_in_profits,my_month_with_greatest_decrease_in_profits])
 
 
 #===========================================================================================================
-# This block of logic writes the output to a TXT file:
+# This block of logic writes the output to a TXT file.
 #===========================================================================================================
-  
 
 output_file = os.path.join('C:/Users/firdo/Desktop/CSV_TEST', 'budget_data_analysis_output.txt')
 
@@ -158,3 +140,7 @@ with open(output_file, "w") as datafile:
     datafile.write("\n")
     datafile.write("Greatest Decrease in Profits: " + my_month_with_greatest_decrease_in_profits + " : " + str('${:,}'.format(my_greatest_decrease_in_profits)))
 
+
+#===========================================================================================================
+# This is the end of the script.
+#===========================================================================================================
